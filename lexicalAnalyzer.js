c0304be1d3504cb87a940b9834de793d68ea718e
Iuri Dias <iuridiasemiliano@hotmail.com>
2020-06-25 18:46:03 -0300

    Lexical analyzer implemented

--- lexicalAnalyzer.js
+++ lexicalAnalyzer.js
@@ -13,131 +13,190 @@ class LexicalAnalyser {
   }
   
   start() {
-    if (this.verbose) console.log('Lexical analyzer started sucessfully.\n');
+    if (this.verbose) console.log('Lexical analyzer started sucessfully.');
 
-    let steps = filtered_steps(this.goldConfig.stepsArray);
+    try {
+      mountLexicalSet(this);
+    } catch(err) {
+      console.log('\nError when mounting the lexical set.\n', err);
+    }
+  }
+}
 
-    let current_state;
-    let previous_state;
-    let next_state;
+function mountLexicalSet(configObj) {
+  let steps = filtered_steps(configObj.goldConfig.stepsArray);
+
+  let current_state;
+  let previous_state;
+  let next_state;
     
-    let line;
-    let char;
-    let previous_char;
+  let row;
+  let char;
+  let previous_char;
     
-    let new_token = true;
-    let token_init;
-    let lexeme;
+  let new_token = true;
+  let token_init;
+  let lexeme;
 
-    for (let line_index in this.sourceCode) {
-      if (!new_token) {
-        let token = this.goldConfig.final_states_tokens[this.goldConfig.final_states.indexOf(current_state)];
-
-        this.resultSet.push({
+  for (let row_index in configObj.sourceCode) {
+    if (!new_token) {
+      if (lexeme[0] == "'") {
+        configObj.errors.push({
+          error: 'Lexical error',
+          error_code: 1,
+          row: +row_index,
+          column: token_init,
+          incorrect_lexeme: lexeme
+        });
+      } else {
+        let token = configObj.goldConfig.final_states_tokens[
+          configObj.goldConfig.final_states.indexOf(current_state)
+        ];
+  
+        configObj.resultSet.push({
           token: token,
           lexeme: lexeme,
-          line: +line_index,
+          row: +row_index, // parseInt(row_index)
           column: token_init
         });
       }
+    }
 
-      line = this.sourceCode[line_index];
-      char = '\n';
-      new_token = true;
+    row = configObj.sourceCode[row_index];
+    char = '\n';
+    new_token = true;
+    
+    for (let char_index = 0; char_index < row.length; char_index++) {
+      if (new_token) {
+        previous_state = null;
+        current_state = configObj.goldConfig.initial_state[0];
+        next_state = null;
+
+        token_init = char_index + 1;
+        lexeme = '';
+      }
       
-      for (let char_index = 0; char_index < line.length; char_index++) {
-        if (new_token) {
-          previous_state = null;
-          current_state = this.goldConfig.initial_state[0];
-          next_state = null;
+      previous_char = char;
+      // Seleciona um novo caractere
+      char = row[char_index];
 
-          token_init = char_index + 1;
-          lexeme = '';
-        }
+      // Busca todos caracteres disponíveis para estado atual
+      // com excessão daqueles que são chaves de regex
+      // Se o caractere for válido segue para o próximo estado
+      if (steps.hasOwnProperty(current_state)
+        && !configObj.goldConfig.regex.hasOwnProperty(char)
+        && steps[current_state].includes(char)) {
+        lexeme = lexeme + char;
+        new_token = false;
         
+        next_state = configObj.goldConfig.stepsObj[current_state + ',' + char];
+        previous_state = current_state;
+        current_state = next_state;
+      
+      // Trata as situações em que o caractere
+      // não é válido para o estado atual
+      } else {
+        // Flag utilizada para verificar se o caractere
+        // é válido em um grupo de regex
+        let char_accepted = false;
 
-        previous_char = char;
-        char = line[char_index];
+        // Verifica se o caractere se enquadra nos grupos de regex 
+        // L, N, # ou . sendo que (L e N podem ser # ou .)
+        if (steps.hasOwnProperty(current_state)) {
+          for (let prop in configObj.goldConfig.regex) {
+            // Se o caractere for válido segue para o próximo estado 
+            if (steps[current_state].includes(prop)
+              && configObj.goldConfig.regex[prop].test(char)) {
+              lexeme = lexeme + char;
+              new_token = false;
 
-        if (steps.hasOwnProperty(current_state) && steps[current_state].includes(char)) {
-          lexeme = lexeme + char;
-          new_token = false;
-          
-          next_state = this.goldConfig.stepsObj[current_state + ',' + char];
-          previous_state = current_state;
-          current_state = next_state;
-        } else {
-          let char_accepted = false;
+              next_state = configObj.goldConfig.stepsObj[current_state + ',' + prop];
+              previous_state = current_state;
+              current_state = next_state;
 
-          if (steps.hasOwnProperty(current_state)) {
-            for (let prop in this.goldConfig.regex) {
-              if (steps[current_state].includes(prop) && this.goldConfig.regex[prop].test(char)) {
-                lexeme = lexeme + char;
-                new_token = false;
-  
-                next_state = this.goldConfig.stepsObj[current_state + ',' + prop];
-                previous_state = current_state;
-                current_state = next_state;
-  
-                char_accepted = true;
-  
-                break;
-              }
+              char_accepted = true;
+
+              break;
             }
           }
+        }
 
-          if (!char_accepted) {
-            if (!this.goldConfig.regex['WS'].test(char)) {
-              if (this.goldConfig.final_states.includes(current_state)) {
-                let token = this.goldConfig.final_states_tokens[this.goldConfig.final_states.indexOf(current_state)];
-                
-                this.resultSet.push({
-                  token: token,
-                  lexeme: lexeme,
-                  line: +line_index + 1,
-                  column: token_init
-                });
+        if (!char_accepted) {
+          // Verifica se o caractere não é um white space
+          if (!configObj.goldConfig.regex['WS'].test(char)) {
+            // Se o caractere for válido segue para o próximo estado
+            if (configObj.goldConfig.final_states.includes(current_state)) {
+              // Insere o token no conjunto resultado e inicia um novo token
+              // verificando novamente o caractere atual
+              let token = configObj.goldConfig.final_states_tokens[
+                configObj.goldConfig.final_states.indexOf(current_state)
+              ];
+              
+              configObj.resultSet.push({
+                token: token,
+                lexeme: lexeme,
+                row: +row_index + 1, // parseInt(row_index + 1)
+                column: token_init
+              });
 
-                new_token = true;
-                char_index--;
-              } else {
-                this.errors.push({
-                  error: `Erro léxico: token inválido na linha ${+line_index + 1} coluna ${token_init}.`,
-                  incorrect_lexeme:  lexeme + char
-                });
-
-                new_token = true;
-              }
+              new_token = true;
+              char_index--;
             } else {
-              if (this.goldConfig.final_states.includes(current_state)) {
-                let token = this.goldConfig.final_states_tokens[this.goldConfig.final_states.indexOf(current_state)];
-                
-                this.resultSet.push({
-                  token: token,
-                  lexeme: lexeme,
-                  line: +line_index + 1,
-                  column: token_init
-                });
+              // Insere o caractere no conjunto de erros e inicia um novo token
+              
+              configObj.errors.push({
+                error: 'Lexical error',
+                error_code: 0,
+                row: +row_index + 1,
+                column: token_init,
+                incorrect_lexeme: lexeme + char
+              });
 
-                new_token = true;
-              } else if (!this.goldConfig.regex['WS'].test(previous_char)) {
-                this.errors.push({
-                  error: `Erro léxico: token inválido na linha ${+line_index + 1} coluna ${token_init}.`,
-                  incorrect_lexeme:  lexeme + char
-                });
+              new_token = true;
+            }
 
-                new_token = true;
-              }
+          // Se o caractere for um white space
+          } else {
+            // Verifica se finalizou um token válido
+            if (configObj.goldConfig.final_states.includes(current_state)) {
+              // Insere o token no conjunto resultado e inicia um novo token
+
+              let token = configObj.goldConfig.final_states_tokens[
+                configObj.goldConfig.final_states.indexOf(current_state)
+              ];
+              
+              configObj.resultSet.push({
+                token: token,
+                lexeme: lexeme,
+                row: +row_index + 1, // parseInt(row_index + 1)
+                column: token_init
+              });
+
+              new_token = true;
+
+            // Se não finalizou um token válido e o último caractere
+            // também não é um white space
+            } else if (!configObj.goldConfig.regex['WS'].test(previous_char)) {
+              // Insere o caractere no conjunto de erros e inicia um novo token
+              
+              configObj.errors.push({
+                error: 'Lexical error',
+                error_code: 0,
+                row: +row_index + 1,
+                column: token_init,
+                incorrect_lexeme: lexeme + char
+              });
+
+              new_token = true;
             }
           }
         }
       }
     }
-
-    if (this.listTokens) console.log(this.resultSet);
-    console.log(this.errors);
-    console.log('\n');
   }
+
+  if (configObj.listTokens)
+    listTokens(configObj.resultSet, configObj.errors);
 }
 
 function filtered_steps(stepsArray) {
@@ -151,6 +210,47 @@ function filtered_steps(stepsArray) {
   }
 
   return filtered_steps;
+}
+
+function listTokens(resultSet, errors) {
+  if (resultSet.length) {
+    let ws = ' ';
+    console.log('\n');
+    console.log(
+      '|       \x1b[31m TOKEN \x1b[0m        ' + 
+      '| \x1b[32m LEXEME \x1b[0m ' +
+      '| \x1b[33m ROW \x1b[0m ' +
+      '| \x1b[33m COLUMN \x1b[0m |');
+    console.log('-'.repeat(54));
+
+    for (let item of resultSet)
+      console.log(
+        `|  \x1b[31m${item.token}\x1b[0m${ws.repeat(18 - item.token.length)}  |  \x1b[32m${item.lexeme}\x1b[0m  |  \x1b[33m${item.row}\x1b[0m  |  \x1b[33m${item.column}\x1b[0m  |`
+      );
+
+    if (errors.length) listErrors(errors);
+
+    console.log('\n');
+  }
+}
+
+function listErrors(errors) {
+  console.log('\n-- Errors --');
+
+  for (let item of errors) {
+    switch (item.error_code) {
+      case 0:
+        console.log(
+          `${item.error}: The token associated with the lexeme (\x1b[32m${item.incorrect_lexeme}\x1b[0m) of row \x1b[33m${item.row}\x1b[0m and column \x1b[33m${item.column}\x1b[0m could not be found.`
+        );
+        break;
+      case 1:
+        console.log(
+          `${item.error}: The end of the string lexeme (\x1b[32m'\x1b[0m) could not be found in lexeme (\x1b[32m${item.incorrect_lexeme}\x1b[0m) of row \x1b[33m${item.row}\x1b[0m and column \x1b[33m${item.column}\x1b[0m.`
+        );
+        break;
+    }    
+  }
 }
 
 module.exports = LexicalAnalyser;
